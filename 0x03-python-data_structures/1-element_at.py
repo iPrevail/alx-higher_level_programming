28f4d8746a01adc48a15e8e8cc51c35dbcbe4796
iPrevail <prevailerkk@gmail.com>
2024-03-17 17:19:00 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """Return the index of a list at position idx
-
-    >>> mylist = [0,1,2,3,4,5]
-    >>> element_at(mylist, 3)
-    3
-    """
-    if idx < len(my_list) and idx >= 0:
-        return my_list[idx]
-
-if __name__=='__main__':
-    import doctest
-    doctest.testmod()
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
