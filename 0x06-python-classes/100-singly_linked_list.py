338451688244819de3909bfe1d9b6d80a5491bf1
iPrevail <prevailerkk@gmail.com>
2024-04-02 15:12:45 -0700

    modified 6 and 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,42 +1,82 @@
 #!/usr/bin/python3
-"""Create a Singly LinkedList in Python"""
+"""module for a singly linked list"""
+
 
 class Node:
-    """Create structure of a singly linkedlist"""
+    """"defines a node"""
 
     def __init__(self, data, next_node=None):
-        """Construct a node object"""
+        """initializes the node with instance variables"""
+
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Property to retrieve data from a node"""
-        return self.__data
+        """gets data attribute"""
 
-    @property
-    def next_node(self):
-        """Property to retrieve the next node"""
-        return self.__next_node
+        return (self.__data)
 
     @data.setter
     def data(self, value):
-        """Change the value in data field"""
+        """sets data attribute"""
+
         if not isinstance(value, int):
             raise TypeError('data must be an integer')
         self.__data = value
 
+    @property
+    def next_node(self):
+        """get next_node attribute
+        Returns: next node
+        """
+
+        return (self.__next_node)
+
     @next_node.setter
     def next_node(self, value):
-        """Change the value of next_node"""
-        if not isinstance(value, Node) or not isinstance(value, NoneType):
+        """set value of next node"""
+
+        if (value is not None and not isinstance(value, Node)):
             raise TypeError('next_node must be a Node object')
-        self.__node = value
+
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Create a singly linkedlist"""
+    """defines a singly linked list"""
 
     def __init__(self):
-        """Simple initializer"""
-        self.__head = Node()
+        """Initializes the singly linked list"""
+
+        self.head = None
+
+    def __str__(self):
+        """make list printable"""
+
+        printsll = ""
+        location = self.head
+        while location:
+            printsll += str(location.data) + "\n"
+            location = location.next_node
+        return printsll[:-1]
+
+    def sorted_insert(self, value):
+        """insert in a sorted fashion
+        Args:
+            value: what the value will be on the node
+        """
+        new = Node(value)
+        if not self.head:
+            self.head = new
+            return
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+        location = self.head
+        while location.next_node and location.next_node.data < value:
+            location = location.next_node
+        if location.next_node:
+            new.next_node = location.next_node
+        location.next_node = new
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,82 +1,83 @@
 #!/usr/bin/python3
-"""Create a class of complex square"""
+"""My square module"""
 
 
 class Square:
-    """Define a square"""
-    def __init__(self, size=0, position=(0, 0)):
+    """defines a square"""
 
-        """Initialize the size of the square, raises TypeError
-        if size is not an int
+    def __init__(self, size=0, position=(0, 0)):
+        """Create a Square
         Args:
-            size: the size of the square"""
+            size: length of a side of Square
+            position: where the square is (coordinates)
+        """
         self.size = size
-        self.__position = position
         self.position = position
 
-    def area(self):
-        """Compute the area of the square
-        Return: the area of the square"""
+    def __str__(self):
+        self.my_print()
 
-        return self.__size**2
+    @property
+    def size(self):
+        """"The propery of size as the len of a side of Square
+        Raises:
+            TypeError: if size != int
+            ValueError: if size < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
-        """Return the tuple whose value is in protected value position"""
+        """property of the coordinates of this Square
+        Raises:
+            TypeError: if value != a tuple of 2 integers < 0
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Set/change the value of the variable in position
-        Args:
-            value: the value of the position field; it must be a tuple
-        Raise:
-            TypeError - if the variable is not tuple
+        """set the position of this Square
+        Args: value as a tuple of two positive integers
+        Raises:
+            TypeError: if value is not a tuple or any int in tuple < 0
         """
         if not isinstance(value, tuple):
             raise TypeError('position must be a tuple of 2 positive integers')
         if len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
-            self.__position = value
+        self.__position = value
 
-    @property
-    def size(self):
-        """Return the value stored in variable size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """change the value stored in private variable size
-        Args:
-            value: new value of size
+    def area(self):
+        """Get the area of a Square
+        Returns: The size squared
         """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+        return self.__size * self.__size
+
+    def pos_print(self):
+        """returns the position in spaces"""
+        pos = ""
+        if self.size == 0:
+            return "\n"
+        for w in range(self.position[1]):
+            pos += "\n"
+        for w in range(self.size):
+            for i in range(self.position[0]):
+                pos += " "
+            for j in range(self.size):
+                pos += "#"
+            pos += "\n"
+        return pos
 
     def my_print(self):
-        """Print the square out using '#'"""
-        i = 0
-        while i < self.size:
-            j, x = 0, 0
-            for a in range(0, self.position[1]):
-                print("")
-            if self.position[1] <= 0:
-                while x < self.position[0]:
-                    print(" ", end="")
-                    x += 1
-
-            while j < self.size:
-                print("#", end="")
-                j += 1
-            print("")
-            i += 1
-
-        if self.__size == 0:
-            print("")
+        """print the square in position"""
+        print(self.pos_print(), end='')
