71d4ad7aad7f23a0dffb58d1eda555edf93622c3
iPrevail <prevailerkk@gmail.com>
2024-04-13 10:23:40 -0700

    corections

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""Test max_integer function"""
-max_integer = __import__('6-max_integer').max_integer
-import unittest
+"""Module to find the max integer in a list
+"""
 
 
-class TestMaxInteger(unittest.TestCase):
-    """Test defining test methods"""
-    
-    def test_wrong(self):
-        """Tests for the wrong input"""
-        self.assertRaises(TypeError, max_integer, "Never mind")
-        self.assertRaises(TypeError, max_integer, None)
-        self.assertRaises(ValueError, maxInteger, ['1', 'None', 3])
-
-    def test_list(self):
-        self.assertEqual(max_integer([1]), 1)
-        self.assertIs(max_integer(), None)
-        self.assertEqual(max_integer([1, 4, 3, -8]), 4)
-        self.assertEqual(max_integer([-4, -7]), -4)
-
-
-if __name__=='__main__':
-    unittest.main()
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
